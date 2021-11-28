9c675c6eef7a6969abab146866b57fd77e43028a
unknown <abdullashafi5@gmail.com>
2021-11-28 13:01:40 +0600

    adding Procfile , debug false

--- ultimateChat/settings.py
+++ ultimateChat/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)g=p!g%&8b_0&jh%2tx5jie7swoj^!)rb*&=a4rum!zt!vdi)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://firenice.herokuapp.com/']
 
 
 # Application definition
